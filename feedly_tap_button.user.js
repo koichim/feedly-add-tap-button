c1f76cc2f7417aaade5436d966a1c5842ed0c099
koichim <koichi@masuda.sppd.ne.jp>
2021-01-02 17:22:37 +0900

    fixed key pressing...
    Looks like webkit bug was fixed.
    https://bugs.webkit.org/show_bug.cgi?id=16735

--- feedly_tap_button.user.js
+++ feedly_tap_button.user.js
@@ -3,11 +3,11 @@
 // @author         Koichi Masuda - koichi@masuda.sppd.ne.jp
 // @namespace      http://masuda.sppd.ne.jp/misc/feedly/
 // @description    Add tap buttons
-// @require 　　 https://code.jquery.com/jquery-2.0.0.min.js
+// @require 　　 https://code.jquery.com/jquery-3.4.1.min.js
 // @include        http://feedly.com/*
 // @include        https://feedly.com/*
-// @date          2020-09-21
-// @version       0.0.10
+// @date          2021-01-02
+// @version       0.0.11
 // ==/UserScript==
 (function() {
     var DEFAULT_MAX_NUM_OF_TABS = 20; // it was 40;
@@ -40,6 +40,28 @@
             thread.processNextEvent(true);
         }
         window.clearInterval(interval);
+    }
+
+    function fire_key_event(trigger, key, keycode) {
+        // https://www.366service.com/jp/qa/96239a5ba278e91a8d08e3e664744859
+        let key_event = new KeyboardEvent(trigger, {'bubbles':true,
+                                                   'key': key,
+                                                   'keyCode': keycode});
+//        Object.defineProperty(key_event, 'charCode', {get:function(){return this.charCodeVal;}});
+//        key_event.charCodeVal = keycode;
+//        delete key_event.charCode;
+//        delete key_event.keyCode;
+//        delete key_event.keyIdentifier;
+//        delete key_event.which;
+//        delete key_event.key;
+//        Object.defineProperties(key_event, {
+//            charCode: {value: keycode},
+//            keyCode: {value: keycode},
+//            keyIdentifier: {value: key},
+//            which: {value: keycode},
+//            key: {value: 'Key'+key}
+//        });
+        document.body.dispatchEvent(key_event);
     }
 
     function add_tap_buttons() {
@@ -80,18 +102,10 @@
 //        var k_event = document.createEvent("KeyboardEvent");
 //        k_event.initKeyEvent("keydown", true, true, null, false, false, false, false, 75, 0);
         k_button.onclick = function(){
-            console.log("k.onclick. mimic p&o pressed");
-//            var k_event = document.createEvent("KeyboardEvent");
-//            k_event.initKeyEvent("keypress", true, true, null, false, false, false, false, 0, 107);
-//            document.getElementsByTagName('body')[0].dispatchEvent(k_event);
-
-            let key_event = new KeyboardEvent('keypress', {bubbles:true});
-            Object.defineProperty(key_event, 'charCode', {get:function(){return this.charCodeVal;}});
-//            key_event.charCodeVal = 107; // k
-            key_event.charCodeVal = 112; // p
-            document.body.dispatchEvent(key_event);
-            key_event.charCodeVal = 111; // o
-            document.body.dispatchEvent(key_event);
+            console.log("k.onclick. mimic k pressed");
+            fire_key_event('keydown', 'k', KeyEvent.DOM_VK_K);
+//            fire_key_event('keydown', 'p', 112);
+//            fire_key_event('keypress', 'o', 111);
         };
 
         var j_button = document.createElement('div');
@@ -102,18 +116,13 @@
         j_button.innerHTML = "j";
         j_button.style.background = "#6666FF";
         j_button.onclick = function(){
-            console.log("j.onclick. mimic n&o pressed");
-//            var j_event = document.createEvent("KeyboardEvent");
-//            j_event.initKeyEvent("keypress", true, true, null, false, false, false, false, 0, 106);
-//            document.getElementsByTagName('body')[0].dispatchEvent(j_event);
-
-            let key_event = new KeyboardEvent('keypress', {bubbles:true});
-            Object.defineProperty(key_event, 'charCode', {get:function(){return this.charCodeVal;}});
-//            key_event.charCodeVal = 106; // j
-            key_event.charCodeVal = 110; // n
-            document.body.dispatchEvent(key_event);
-            key_event.charCodeVal = 111; // o
-            document.body.dispatchEvent(key_event);
+            let key_event;
+            console.log("j.onclick. mimic j pressed");
+//            fire_key_event('keydown', 'j', 106);
+            fire_key_event('keydown', 'j', KeyEvent.DOM_VK_J);
+//            fire_key_event('keydown', 'n', 110);
+//            fire_key_event('keypress', 'o', 111);
+            return;
         };
 
         var n_button = document.createElement('div');
@@ -125,13 +134,7 @@
         n_button.style.background = "#F3F781";
         n_button.onclick = function(){
             console.log("n.onclick. mimic n pressed");
-//            var n_event = document.createEvent("KeyboardEvent");
-//            n_event.initKeyEvent("keypress", true, true, null, false, false, false, false, 0, 110);
-//            document.getElementsByTagName('body')[0].dispatchEvent(n_event);
-            var n_event = new KeyboardEvent('keypress', {bubbles:true});
-            Object.defineProperty(n_event, 'charCode', {get:function(){return this.charCodeVal;}});
-            n_event.charCodeVal = 110;
-            document.body.dispatchEvent(n_event);
+            fire_key_event('keydown', 'n', KeyEvent.DOM_VK_N);
         };
 
 
@@ -214,5 +217,123 @@
     //	    add_tap_buttons();
     //	}
     //    };
-
+    if (typeof KeyEvent == "undefined") {
+        var KeyEvent = {
+            DOM_VK_CANCEL: 3,
+            DOM_VK_HELP: 6,
+            DOM_VK_BACK_SPACE: 8,
+            DOM_VK_TAB: 9,
+            DOM_VK_CLEAR: 12,
+            DOM_VK_RETURN: 13,
+            DOM_VK_ENTER: 14,
+            DOM_VK_SHIFT: 16,
+            DOM_VK_CONTROL: 17,
+            DOM_VK_ALT: 18,
+            DOM_VK_PAUSE: 19,
+            DOM_VK_CAPS_LOCK: 20,
+            DOM_VK_ESCAPE: 27,
+            DOM_VK_SPACE: 32,
+            DOM_VK_PAGE_UP: 33,
+            DOM_VK_PAGE_DOWN: 34,
+            DOM_VK_END: 35,
+            DOM_VK_HOME: 36,
+            DOM_VK_LEFT: 37,
+            DOM_VK_UP: 38,
+            DOM_VK_RIGHT: 39,
+            DOM_VK_DOWN: 40,
+            DOM_VK_PRINTSCREEN: 44,
+            DOM_VK_INSERT: 45,
+            DOM_VK_DELETE: 46,
+            DOM_VK_0: 48,
+            DOM_VK_1: 49,
+            DOM_VK_2: 50,
+            DOM_VK_3: 51,
+            DOM_VK_4: 52,
+            DOM_VK_5: 53,
+            DOM_VK_6: 54,
+            DOM_VK_7: 55,
+            DOM_VK_8: 56,
+            DOM_VK_9: 57,
+            DOM_VK_SEMICOLON: 59,
+            DOM_VK_EQUALS: 61,
+            DOM_VK_A: 65,
+            DOM_VK_B: 66,
+            DOM_VK_C: 67,
+            DOM_VK_D: 68,
+            DOM_VK_E: 69,
+            DOM_VK_F: 70,
+            DOM_VK_G: 71,
+            DOM_VK_H: 72,
+            DOM_VK_I: 73,
+            DOM_VK_J: 74,
+            DOM_VK_K: 75,
+            DOM_VK_L: 76,
+            DOM_VK_M: 77,
+            DOM_VK_N: 78,
+            DOM_VK_O: 79,
+            DOM_VK_P: 80,
+            DOM_VK_Q: 81,
+            DOM_VK_R: 82,
+            DOM_VK_S: 83,
+            DOM_VK_T: 84,
+            DOM_VK_U: 85,
+            DOM_VK_V: 86,
+            DOM_VK_W: 87,
+            DOM_VK_X: 88,
+            DOM_VK_Y: 89,
+            DOM_VK_Z: 90,
+            DOM_VK_CONTEXT_MENU: 93,
+            DOM_VK_NUMPAD0: 96,
+            DOM_VK_NUMPAD1: 97,
+            DOM_VK_NUMPAD2: 98,
+            DOM_VK_NUMPAD3: 99,
+            DOM_VK_NUMPAD4: 100,
+            DOM_VK_NUMPAD5: 101,
+            DOM_VK_NUMPAD6: 102,
+            DOM_VK_NUMPAD7: 103,
+            DOM_VK_NUMPAD8: 104,
+            DOM_VK_NUMPAD9: 105,
+            DOM_VK_MULTIPLY: 106,
+            DOM_VK_ADD: 107,
+            DOM_VK_SEPARATOR: 108,
+            DOM_VK_SUBTRACT: 109,
+            DOM_VK_DECIMAL: 110,
+            DOM_VK_DIVIDE: 111,
+            DOM_VK_F1: 112,
+            DOM_VK_F2: 113,
+            DOM_VK_F3: 114,
+            DOM_VK_F4: 115,
+            DOM_VK_F5: 116,
+            DOM_VK_F6: 117,
+            DOM_VK_F7: 118,
+            DOM_VK_F8: 119,
+            DOM_VK_F9: 120,
+            DOM_VK_F10: 121,
+            DOM_VK_F11: 122,
+            DOM_VK_F12: 123,
+            DOM_VK_F13: 124,
+            DOM_VK_F14: 125,
+            DOM_VK_F15: 126,
+            DOM_VK_F16: 127,
+            DOM_VK_F17: 128,
+            DOM_VK_F18: 129,
+            DOM_VK_F19: 130,
+            DOM_VK_F20: 131,
+            DOM_VK_F21: 132,
+            DOM_VK_F22: 133,
+            DOM_VK_F23: 134,
+            DOM_VK_F24: 135,
+            DOM_VK_NUM_LOCK: 144,
+            DOM_VK_SCROLL_LOCK: 145,
+            DOM_VK_COMMA: 188,
+            DOM_VK_PERIOD: 190,
+            DOM_VK_SLASH: 191,
+            DOM_VK_BACK_QUOTE: 192,
+            DOM_VK_OPEN_BRACKET: 219,
+            DOM_VK_BACK_SLASH: 220,
+            DOM_VK_CLOSE_BRACKET: 221,
+            DOM_VK_QUOTE: 222,
+            DOM_VK_META: 224
+        };
+    }
 })();
